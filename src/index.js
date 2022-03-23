ef9a462a5d29d1b6b959a5a19ddd32ff31b208f3
LeonAdeoye <leon.adeoye@gmail.com>
2022-03-23 17:59:58 +0800

    clock

--- src/index.js
+++ src/index.js
@@ -157,16 +157,20 @@ class Game extends React.Component
         let status;
         if(winner)
         {
-            status = 'Congratulations! The winner is: ' + winner;
+            status = <h1>{'Congratulations! The winner is: ' + winner}</h1>;
         }
         else
         {
             // Change the “status” text in Board’s render so that it displays which player has the next turn:
             if(calculateDraw(current.squares))
-                status = 'This is a draw!!!'
+                status = <h2>'This is a draw!!!'</h2>
             else
-                status = 'The next player is: ' + (this.state.xIsNext ? 'X' : 'O') ;
+                status = <h3>{'The next player is: ' + (this.state.xIsNext ? 'X' : 'O')}</h3>;
         }
+
+        // When using React, you generally don’t need to call addEventListener to add listeners to a DOM element after it is created.
+        // Instead, just provide a listener when the element is initially rendered.
+        // When you define a component using an ES6 class, a common pattern is for an event handler to be a method on the class.
 
         return (
             <div className="game">
@@ -175,6 +179,7 @@ class Game extends React.Component
                            onClick={(i) => this.handleClick(i)} />
                 </div>
                 <div className="game-info">
+                    <div><Clock/></div>
                     <div>{status}</div>
                     <ol>{moves}</ol>
                 </div>
@@ -183,9 +188,76 @@ class Game extends React.Component
     }
 }
 
+// This is commonly called a “top-down” or “unidirectional” data flow.
+// Any state is always owned by some specific component, and any data or UI derived from that state can only affect components “below” them in the tree.
+
+function FormattedDate(props)
+{
+    return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
+// When <Clock /> is passed to ReactDOM.render(), React calls the constructor of the Clock component.
+// Since Clock needs to display the current time, it initializes this.state with an object including the current time. We will later update this state.
+// React then calls the Clock component’s render() method.
+// This is how React learns what should be displayed on the screen. React then updates the DOM to match the Clock’s render output.
+// When the Clock output is inserted in the DOM, React calls the componentDidMount() lifecycle method.
+// Inside it, the Clock component asks the browser to set up a timer to call the component’s tick() method once a second.
+// Every second the browser calls the tick() method. Inside it, the Clock component schedules a UI update by calling setState() with an object containing the current time.
+// Thanks to the setState() call, React knows the state has changed, and calls the render() method again to learn what should be on the screen.
+// This time, this.state.date in the render() method will be different, and so the render output will include the updated time. React updates the DOM accordingly.
+// If the Clock component is ever removed from the DOM, React calls the componentWillUnmount() lifecycle method so the timer is stopped.
+
+class Clock extends React.Component
+{
+    // Class components should always call the base constructor with props.
+    constructor(props)
+    {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    //  Make the Clock component set up its own timer and update itself every second.
+    // In applications with many components, it’s very important to free up resources taken by the components when they are destroyed.
+    // We want to set up a timer whenever the Clock is rendered to the DOM for the first time. This is called “mounting” in React.
+    // We also want to clear that timer whenever the DOM produced by the Clock is removed. This is called “unmounting” in React.
+    // The componentDidMount() lifecycle method runs after the component output has been rendered to the DOM. This is a good place to set up a timer.
+    // While this.props is set up by React itself and this.state has a special meaning,
+    // you are free to add additional fields to the class manually if you need to store something that does not participate in the data flow (like a timer ID).
+
+    componentDidMount()
+    {
+        this.timerID = setInterval(() => this.tick(),1000);
+    }
+
+    // We will tear down the timer in the componentWillUnmount() lifecycle method.
+    componentWillUnmount()
+    {
+        clearInterval(this.timerID);
+    }
+
+    // We will implement a method called tick() that the Clock component will run every second.
+    // It will use this.setState() to schedule updates to the component local state
+    tick()
+    {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render()
+    {
+        return (
+            <div>
+                <FormattedDate date={this.state.date} />
+            </div>
+        );
+    }
+}
+
+
 // ========================================
 
 ReactDOM.render(
-    <Game />,
+    <Game/>,
     document.getElementById('root')
 );
