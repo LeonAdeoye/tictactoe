3e7738805a074e9f5e033b9cf2e063601c8e759e
LeonAdeoye <leon.adeoye@gmail.com>
2022-03-24 20:35:21 +0800

    list mgmt

--- src/index.js
+++ src/index.js
@@ -144,9 +144,7 @@ class Game extends React.Component
         // The button has a onClick handler which calls a method called this.jumpTo().
         const moves = history.map((step, move) =>
         {
-            const desc = move ?
-                'Go to move #' + move :
-                'Go to game start';
+            const desc = move ? ('Go to move #' + move) : ('Go to game start');
             return (
                 <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
@@ -171,6 +169,7 @@ class Game extends React.Component
         // When using React, you generally don’t need to call addEventListener to add listeners to a DOM element after it is created.
         // Instead, just provide a listener when the element is initially rendered.
         // When you define a component using an ES6 class, a common pattern is for an event handler to be a method on the class.
+        const numbers = [10, 11, 12, 13]
 
         return (
             <div className="game">
@@ -179,6 +178,8 @@ class Game extends React.Component
                            onClick={(i) => this.handleClick(i)} />
                 </div>
                 <div className="game-info">
+                    <NumberList numbers={[1,2,3,4,5]}/>
+                    <NumberList numbers={numbers}/>
                     <LoginControl></LoginControl>
                     <Toggle />
                     <Clock/>
@@ -364,6 +365,41 @@ function LogoutButton(props)
         <button onClick={props.onClick}>
             Logout
         </button>
+    );
+}
+
+// Each list item needs a “key” which is a special string attribute you need to include when creating lists of elements without which you will get warnings in DevTools console.
+// Keys help React identify which items have changed, are added, or are removed. Keys should be given to the elements inside the array to give the elements a stable identity.
+// A good rule of thumb is that elements inside the map() call need keys.
+function NumberList(props)
+    {
+    const numbers = props.numbers;
+
+    // The best way to pick a key is to use a string that uniquely identifies a list item among its siblings.
+    // This approach is preferred to using the index below.
+    const listItems = numbers.map((number) =>
+        <li key={number.toString()}>
+            {number}
+        </li>
+    );
+
+    // When you don’t have stable IDs for rendered items, you may use the item index as a key as a last resort.
+    // We don’t recommend using indexes for keys if the order of items may change. This can negatively impact performance and may cause issues with component state.
+    const todoItems = numbers.map((number, index) =>
+        // Only do this if items have no stable IDs
+        <li key={index}>
+            {number}
+        </li>
+    );
+
+    // JSX allows embedding any expression in curly braces so we could inline the map() result.
+    return (
+        <ul>{ numbers.map((number, index) =>
+            // Only do this if items have no stable IDs
+            <li key={index}>
+                {number}
+            </li> )}
+        </ul>
     );
 }
 
