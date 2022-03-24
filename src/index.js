c455e9228e386db88f0b87683684ace5b2de8da7
LeonAdeoye <leon.adeoye@gmail.com>
2022-03-24 18:09:37 +0800

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -179,8 +179,9 @@ class Game extends React.Component
                            onClick={(i) => this.handleClick(i)} />
                 </div>
                 <div className="game-info">
+                    <LoginControl></LoginControl>
                     <Toggle />
-                    <div><Clock/></div>
+                    <Clock/>
                     <div>{status}</div>
                     <ol>{moves}</ol>
                 </div>
@@ -291,6 +292,79 @@ class Toggle extends React.Component
             </button>
         );
     }
+}
+
+class LoginControl extends React.Component {
+    constructor(props)
+    {
+        super(props);
+        // this.handleLoginClick = this.handleLoginClick.bind(this);
+        // this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {isLoggedIn: false};
+    }
+
+    // Prefer to use experimental public class syntax so commenting out above bindings in constructor.
+    handleLoginClick = () =>
+    {
+        this.setState({isLoggedIn: true});
+    }
+
+    // Prefer to use experimental public class syntax so commenting out above bindings in constructor.
+    handleLogoutClick = () =>
+    {
+        this.setState({isLoggedIn: false});
+    }
+
+    render()
+    {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+        button =  isLoggedIn ? <LogoutButton onClick={this.handleLogoutClick} /> : <LoginButton onClick={this.handleLoginClick} />
+        return (
+            <div>
+                <Greeting isLoggedIn={isLoggedIn} />
+                {button}
+            </div>
+        );
+    }
+}
+
+function UserGreeting(props)
+{
+    return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props)
+{
+    return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props)
+{
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn)
+    {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+function LoginButton(props)
+{
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props)
+{
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
 }
 
 // ========================================
