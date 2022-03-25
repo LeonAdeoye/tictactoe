9381e23074dd6193599a5c9304d07418f2deb725
LeonAdeoye <leon.adeoye@gmail.com>
2022-03-25 18:10:53 +0800

    forms

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-// We can combine the two by making the React state be the “single source of truth”.
-// Then the React component that renders a form also controls what happens in that form on subsequent user input.
+// We can make the React component state be the “single source of truth”.
+// The React component that renders a form also controls what happens in that form on subsequent user input.
 // An input form element whose value is controlled by React in this way is called a “controlled component”.
 class NameForm extends React.Component
 {
@@ -9,8 +9,12 @@ class NameForm extends React.Component
     {
         super(props);
         this.state = {value: ''};
-
     }
+
+    // Since the value attribute is set on our form element, the displayed value will always be this.state.value, making the React state the source of truth.
+    // Since handleChange runs on every keystroke to update the React state, the displayed value will update as the user types.
+    // With a controlled component, the input’s value is always driven by the React state.
+    // While this means you have to type a bit more code, you can now pass the value to other UI elements too, or reset it from other event handlers.
 
     handleChange = (event) =>
     {
